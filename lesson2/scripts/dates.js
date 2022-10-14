eadb83228e3371e6a21fed4d330a70c3790803c7
jdharri47 <har20040@byui.edu>
2022-10-14 15:38:48 -0600

    java inclusion

--- lesson2/scripts/dates.js
+++ lesson2/scripts/dates.js
@@ -1,9 +1,8 @@
 const options = {weekday:'long', day:'numeric', month:'long', year:'numeric'};
-
 const todaysDate = new Date();
 
 let year = todaysDate.getFullYear;
 let lastModified = new Date(document.lastModified);
 
 document.querySelector('#lastmodified').textContent = lastmodified.toLocaleDateString('en-US', options);
-document.querySelector('#year').textContent = year
+document.querySelector('#year').textContent = year;
