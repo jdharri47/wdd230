6d5ed09469ae22675825d1ec7b2e47af17b97100
jdharri47 <har20040@byui.edu>
2022-10-13 23:13:56 -0600

    BoM commit

---
+++ script/bom.js
@@ -0,0 +1,29 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+    button.addEventListener('click', function() {
+        if (input.value.length === 0) {
+
+        } else {
+            let myItem = input.value;
+
+            const listItem = document.createElement('li');
+            const listText = document.createElement('span');
+            const listBtn =document.createElement('button');
+
+            listItem.appendChild(listText);
+            listText.textContent = myItem;
+            listItem.appendChild(listBtn);
+            listBtn.textContent = '❌';
+
+            list.appendChild(listItem);
+
+            input.value = '';
+
+            listBtn.addEventListener('click', function(){
+                list.removeChild(listItem);
+            });
+        }
+        input.focus();
+    })
