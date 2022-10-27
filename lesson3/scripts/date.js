f62f6578c64974a5d53bebde6d5794ea5c561e15
jdharri47 <har20040@byui.edu>
2022-10-26 23:54:36 -0600

    Web plan

--- lesson3/scripts/date.js
+++ lesson3/scripts/date.js
@@ -1,5 +1,9 @@
 const options = {weekday:'long', day:'numeric', month:'long', year:'numeric'};
+const todaysDate = new Date();
 
 let lastmodified = new Date(document.lastModified);
 
-document.getElementById('date').textContent = lastmodified.toLocaleDateString('en-US', options);
+let thisYear = todaysDate.getFullYear;
+
+document.querySelector('#year').textContent = thisYear;
+document.querySelector('#lastmodified').textContent = lastmodified.toLocaleDateString('en-US', options);
