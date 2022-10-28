4f13f020b38e4e622291338b43fd5b5798b50ac9
jdharri47 <har20040@byui.edu>
2022-10-27 20:32:41 -0600

    lesson 3 update

--- lesson3/scripts/date.js
+++ lesson3/scripts/date.js
@@ -1,9 +1,9 @@
 const options = {weekday:'long', day:'numeric', month:'long', year:'numeric'};
-const todaysDate = new Date();
+const todaysDate = new Date(Date.now());
 
 let lastmodified = new Date(document.lastModified);
 
-let thisYear = todaysDate.getFullYear;
+let thisYear = todaysDate.getFullYear();
 
 document.querySelector('#year').textContent = thisYear;
-document.querySelector('#lastmodified').textContent = lastmodified.toLocaleDateString('en-US', options);
+document.querySelector('#lastmodified').textContent = lastmodified.toLocaleDateString('en-Uk', options);
